a2cea547825e67e1943b5cf7acc513e8c11df041
yen <l.yennie.j@gmail.com>
2024-02-06 18:19:34 +0900

    Clean up auth page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,18 +4,17 @@ import React, { useState, FormEvent } from "react";
 import { useRouter } from "next/navigation";
 import { Input, Button, Spinner } from "@nextui-org/react";
 
-import { signUp } from "@/data/auth";
+import { logIn } from "@/data/auth";
 import axiosInstance from "@/data/axiosInstance";
 
 import { title } from "@/components/primitives";
 import { EyeFilledIcon, EyeSlashFilledIcon } from "@/components/icons";
 
-function SignUp() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+function LogIn() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
-  const [isSignUpLoading, setIsSignUpLoading] = useState<boolean>(false);
+  const [isLogInLoading, setIsLogInLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
 
   const router = useRouter();
@@ -24,17 +23,12 @@ function SignUp() {
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    if (password !== confirmPassword) {
-      setError("비밀번호가 일치하지 않습니다.");
-      return;
-    }
-    setIsSignUpLoading(true);
+    setIsLogInLoading(true);
 
     try {
-      const authUser = await signUp(email, password);
+      const authUser = await logIn(email, password);
       if (authUser) {
-        const response = await axiosInstance.post("/api/signup", {
+        const response = await axiosInstance.post("/api/login", {
           email,
           password,
         });
@@ -45,13 +39,13 @@ function SignUp() {
     } catch (error: any) {
       setError(error.message);
     } finally {
-      setIsSignUpLoading(false);
+      setIsLogInLoading(false);
     }
   };
 
   return (
     <section className="flex flex-col items-center space-y-8 ">
-      <h1 className={title()}>SignUp</h1>
+      <h1 className={title()}>LogIn</h1>
       <form
         className="w-96 p-10 space-y-10 border-medium border-default-200 rounded"
         onSubmit={handleSubmit}
@@ -75,28 +69,14 @@ function SignUp() {
           type={isPasswordVisible ? "text" : "password"}
           name="password"
           label="password"
-          placeholder="6자리 이상의 비밀번호를 입력해주세요"
+          placeholder="비밀번호를 입력해주세요."
           isRequired
           autoComplete="current-password"
-          variant="bordered"
-          labelPlacement="outside"
-          onValueChange={(changedInput) => {
-            setPassword(changedInput);
-            changedInput && setError("");
-          }}
-        />
-        <Input
-          type={isPasswordVisible ? "text" : "password"}
-          name="confirm-password"
-          label="confirm password"
-          placeholder="다시 한번 비밀번호를 입력해주세요"
-          isRequired
-          autoComplete="confirm-password"
           errorMessage={error}
           variant="bordered"
           labelPlacement="outside"
           onValueChange={(changedInput) => {
-            setConfirmPassword(changedInput);
+            setPassword(changedInput);
             changedInput && setError("");
           }}
           endContent={
@@ -118,27 +98,23 @@ function SignUp() {
           color="warning"
           variant="flat"
           fullWidth
-          isDisabled={
-            email.trim() === "" ||
-            password.trim() === "" ||
-            confirmPassword.trim() === ""
-          }
+          isDisabled={email.trim() === "" || password.trim() === ""}
         >
-          {isSignUpLoading ? <Spinner color="warning" /> : "Sign In"}
+          {isLogInLoading ? <Spinner color="warning" /> : "Sign In"}
         </Button>
       </form>
       <p className="mt-10 text-center text-sm text-gray-400">
-        Already have an account?
+        Not a member?
         <Button
-          onClick={() => router.push("login")}
+          onClick={() => router.push("signup")}
           variant="light"
           className="ml-1"
         >
-          Log In
+          Sign Up
         </Button>
       </p>
     </section>
   );
 }
 
-export default SignUp;
+export default LogIn;
