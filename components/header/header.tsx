caac4925a7587079e542cb50cbcb8c49fcd433db
yen <l.yennie.j@gmail.com>
2024-02-09 17:20:13 +0900

    Add consent

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -17,18 +17,30 @@ import AddHeaderForm from "./add-header-form";
 import EditHeaderForm from "./edit-header-form";
 import DeleteHeaderForm from "./delete-header-form";
 import { FocusHeaderType, Header, HeaderModalType } from "@/types";
+import { speakText, checkTimeAndSpeak } from "@/app/utils/getSpeech";
 
 const Heaer = () => {
+  // 데이터 / 모달 / 모달 데이터 / 음성 동의
   const [header, setHeader] = useState({ startTime: "", description: "" });
-
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [currentModalData, setCurrentModalData] = useState<FocusHeaderType>({
     focusedHeader: null,
     modalType: "addHeader",
   });
+  const [isConsented, setIsConsented] = useState(false);
+
+  const toggleConsent = () => setIsConsented(!isConsented);
+
+  const currentTime = checkTimeAndSpeak(
+    header.startTime,
+    isConsented,
+    header.description
+  );
 
   useEffect(() => {
     fetchHeader();
+    const consent = localStorage.getItem("voiceConsent");
+    setIsConsented(consent === "true");
   }, []);
 
   const fetchHeader = async () => {
@@ -44,7 +56,11 @@ const Heaer = () => {
     }
   };
 
-  const ModalHandler = (header: Header | null, key: HeaderModalType) => {
+  const speechHandler = () => {
+    speakText(header.description);
+  };
+
+  const modalHandler = (header: Header | null, key: HeaderModalType) => {
     onOpen();
     setCurrentModalData({
       focusedHeader: header,
@@ -52,10 +68,20 @@ const Heaer = () => {
     });
   };
 
-  const getModalContent = (modalData: FocusHeaderType, onClose: () => void) => {
+  const renderModalContent = (
+    modalData: FocusHeaderType,
+    onClose: () => void
+  ) => {
     switch (modalData.modalType) {
       case "addHeader":
-        return <AddHeaderForm onClose={onClose} fetchHeader={fetchHeader} />;
+        return (
+          <AddHeaderForm
+            onClose={onClose}
+            fetchHeader={fetchHeader}
+            isConsented={isConsented}
+            toggleConsent={toggleConsent}
+          />
+        );
       case "editHeader":
         return (
           <EditHeaderForm
@@ -75,12 +101,15 @@ const Heaer = () => {
     <div className="border">
       {header.description ? (
         <>
+          <h2>현재시간{currentTime}</h2>
+
+          <button onClick={speechHandler}>speech</button>
           <div className="relative flex justify-end items-center gap-2">
             <Tooltip content="Edit Header">
               <span
                 className="text-lg text-default-400 cursor-pointer active:opacity-50"
                 onClick={() => {
-                  ModalHandler(header, "editHeader");
+                  modalHandler(header, "editHeader");
                 }}
               >
                 <EditIcon />
@@ -90,7 +119,7 @@ const Heaer = () => {
               <span
                 className="text-lg text-danger cursor-pointer active:opacity-50"
                 onClick={() => {
-                  ModalHandler(null, "deleteHeader");
+                  modalHandler(null, "deleteHeader");
                 }}
               >
                 <DeleteIcon />
@@ -119,7 +148,7 @@ const Heaer = () => {
           type="submit"
           color="warning"
           variant="flat"
-          onPress={() => ModalHandler(null, "addHeader")}
+          onPress={() => modalHandler(null, "addHeader")}
         >
           머릿말 추가하기
         </Button>
@@ -132,7 +161,7 @@ const Heaer = () => {
         onOpenChange={onOpenChange}
       >
         <ModalContent>
-          {(onClose) => getModalContent(currentModalData, onClose)}
+          {(onClose) => renderModalContent(currentModalData, onClose)}
         </ModalContent>
       </Modal>
     </div>
