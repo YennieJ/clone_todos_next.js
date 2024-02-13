71d6e0ec4570750eadcd2e4c4e941fb26f5c2bc9
yen <l.yennie.j@gmail.com>
2024-02-13 17:10:50 +0900

    Update meta

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Yennie Routine",
+  description: "Make your routine",
   navItems: [
     {
       label: "Home",
